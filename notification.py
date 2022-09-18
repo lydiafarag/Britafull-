77c84f873596b2a5b141527daf57c079327a7439
Kailin <68765813+kailinchu@users.noreply.github.com>
2022-09-18 00:42:33 -0400

    Add main file that activates sound and notifs when brita is empty

--- notification.py
+++ notification.py
@@ -5,21 +5,21 @@ import json
 
 load_dotenv()
 
-def sendNotfication():
+def sendNotification():
     # Your Account SID from twilio.com/console
     account_sid = os.getenv('TWILIO_ACCOUNT_SID')
     # Your Auth Token from twilio.com/console
     auth_token  = os.getenv('TWILIO_AUTH_TOKEN')
     numbers = json.loads(os.environ['PHONE_NUMBERS'])
-
+    outgoing_number = os.getenv('OUTGOING_NUMBER')
 
     client = Client(account_sid, auth_token)
 
     for num in numbers:
         message = client.messages.create(
             to=num, 
-            from_="+14232642641",
+            from_=outgoing_number,
             body="One of your roommates has not refilled the Brita!")
         print(message.sid)
 
-sendNotfication()
+sendNotification()
